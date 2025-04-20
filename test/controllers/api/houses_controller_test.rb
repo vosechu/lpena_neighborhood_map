5aef4250102c50781cf0e71bbf032ee82749bb58
vosechu <vosechu@gmail.com>
2025-04-20 15:33:21 -0400

    chore: remove test::unit and configure rspec generators

--- test/controllers/api/houses_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Api::HousesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_houses_index_url
-    assert_response :success
-  end
-end
--- test/controllers/pages_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get map" do
-    get pages_map_url
-    assert_response :success
-  end
-end
