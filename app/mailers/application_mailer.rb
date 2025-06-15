9be918ceaba054ff39dfca925e4dc442a510a176
vosechu <vosechu@gmail.com>
2025-06-15 13:11:08 -0400

    Fix deployments

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  include ActiveJob::QueueName  # Required for queue_as in Rails 8.0
   default from: 'no-reply@lakepasadenaestates.com'
   layout 'mailer'
 end
